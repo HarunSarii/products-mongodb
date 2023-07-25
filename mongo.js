00a17553e85859f5bf76da78d78e0d78d2740611
HarunSarii <harunsari14@gmail.com>
2023-07-26 00:28:54 +0300

    feat: add getting products

--- mongo.js
+++ mongo.js
@@ -14,14 +14,29 @@ const createProduct = async (req, res, next) => {
     const db = client.db();
     const result = db.collection("products").insertOne(newProduct);
   } catch (error) {
-    return res.json({ message: "Could not store data.", error });
+    return res.json({ message: "Could not store data." });
   }
   client.close();
 
   res.json(newProduct);
 };
 
-const getProducts = async (req, res, next) => {};
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  let products;
+
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection("products").find().toArray();
+  } catch (error) {
+    return res.json({ message: "Could not retrieve products." });
+  }
+  client.close();
+
+  res.json(products);
+};
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
